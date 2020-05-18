d963fd3db21d5062a850d00bf4d73a62c1bfe252
newcats <newcats@live.com>
2020-05-18 19:34:58 +0800

    添加依赖注册器的示例

---
+++ samples/Newcats.WebApi/IFileStore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Newcats.DenpendencyInjection;
+
+namespace Newcats.WebApi
+{
+    public interface IFileStore
+    {
+        void Save();
+    }
+
+    public class FileStore : IFileStore
+    {
+        public void Save()
+        {
+            Console.WriteLine("hello file");
+        }
+    }
+
+    public class ServiceRegister : IDependencyRegistrar
+    {
+        public void Register(IServiceCollection services)
+        {
+            services.AddScoped<IFileStore, FileStore>();
+            services.AddScoped(typeof(DataAccess.IRepository<,>), typeof(DataAccess.SqlServer.Repository<,>));//注册泛型仓储
+        }
+    }
+}
