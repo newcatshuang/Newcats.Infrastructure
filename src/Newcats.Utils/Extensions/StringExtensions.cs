4c2c43a811802ac549b3fe36e2af37b62ae980f4
newcats <newcats@live.com>
2020-05-31 15:37:22 +0800

    IsNullOrWhiteSpace扩展方法

--- src/Newcats.Utils/Extensions/StringExtensions.cs
+++ src/Newcats.Utils/Extensions/StringExtensions.cs
@@ -450,6 +450,16 @@ namespace Newcats.Utils.Extensions
 
         #region Substring
         /// <summary>
+        /// Indicates whether a specified string is null, empty, or consists only of white-space characters.
+        /// </summary>
+        /// <param name="value">The string to test.</param>
+        /// <returns>true if the value parameter is null or System.String.Empty, or if value consists exclusively of white-space characters.</returns>
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        /// <summary>
         /// Retrieves a substring from this instance. The substring starts at a specified character position and has a specified length.
         /// </summary>
         /// <param name="str">The string</param>
