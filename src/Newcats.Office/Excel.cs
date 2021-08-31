b3e564637cdbc0cccf33755165307e8fd3825cc8
newcats <newcats@live.com>
2021-08-31 15:53:25 +0800

    从Utils项目分离Office操作

--- src/Newcats.Office/Excel.cs
+++ src/Newcats.Office/Excel.cs
@@ -8,7 +8,7 @@ using NPOI.HSSF.UserModel;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 
-namespace Newcats.Utils.Helpers
+namespace Newcats.Office
 {
     public enum ExcelFormat
     {
@@ -45,6 +45,340 @@ namespace Newcats.Utils.Helpers
     /// <summary>
     /// Excel文件帮助类，通过NPOI进行操作
     /// </summary>
+    public static class Excel
+    {
+        /// <summary>
+        /// 读取Excel到DataTable(NPOI方式)(默认获取第一个Sheet)(第一行为表头,不记录数据)
+        /// </summary>
+        /// <param name="fullFilePath">文件的完整物理路径</param>
+        /// <returns>DataTable</returns>
+        public static DataTable ReadExcelToTable(string fullFilePath)
+        {
+            IWorkbook wk = null;
+            using (FileStream file = new FileStream(fullFilePath, FileMode.Open, FileAccess.Read))
+            {
+                file.Position = 0;
+                string fileExtension = Path.GetExtension(file.Name).ToLower();
+                switch (fileExtension)
+                {
+                    case ".xls":
+                        wk = new HSSFWorkbook(file);
+                        break;
+                    case ".xlsx":
+                        wk = new XSSFWorkbook(file);
+                        break;
+                }
+                return GetDataTableFromWorkbook(wk);
+            }
+        }
+
+        /// <summary>
+        /// 读取Excel到DataTable(NPOI方式)(默认获取第一个Sheet)(第一行为表头,不记录数据)
+        /// </summary>
+        /// <param name="file">含有Excel的文件流</param>
+        /// <param name="format">文件扩展名（.xls,.xlsx）</param>
+        /// <returns>DataTable</returns>
+        public static DataTable ReadExcelToTable(Stream file, ExcelFormat format)
+        {
+            file.Position = 0;
+            IWorkbook wk = null;
+            switch (format)
+            {
+                case ExcelFormat.xls:
+                    wk = new HSSFWorkbook(file);
+                    break;
+                case ExcelFormat.xlsx:
+                    wk = new XSSFWorkbook(file);
+                    break;
+            }
+            return GetDataTableFromWorkbook(wk);
+        }
+
+        /// <summary>
+        /// 读取Excel到DataTable(NPOI方式)(默认获取第一个Sheet)(第一行为表头,不记录数据)
+        /// </summary>
+        /// <param name="file">含有Excel的文件</param>
+        /// <returns>DataTable</returns>
+        public static DataTable ReadExcelToTable(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                throw new FileNotFoundException("The excel file do not exist!");
+            IWorkbook wk = null;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                file.CopyTo(stream);
+                stream.Position = 0;
+                switch (file.ContentType.ToLower())
+                {
+                    case "application/vnd.ms-excel":
+                        wk = new HSSFWorkbook(stream);
+                        break;
+                    case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+                        wk = new XSSFWorkbook(stream);
+                        break;
+                }
+                return GetDataTableFromWorkbook(wk);
+            }
+        }
+
+        /// <summary>
+        /// 读取DataTable数据源到Excel内存流(NPOI方式)
+        /// 1.写入到Excel文件，2.输出到响应
+        /// 3.用完之后记得释放（using{}）
+        /// </summary>
+        /// <param name="source">数据源</param>
+        /// <param name="format">文件扩展名（.xls,.xlsx）</param>
+        /// <returns>数据流</returns>
+        public static MemoryStream ReadDataTableToExcel(DataTable source, ExcelFormat format = ExcelFormat.xls)
+        {
+            if (source == null || source.Rows.Count == 0)
+                throw new ArgumentNullException(nameof(source));
+
+            IWorkbook workbook = null;
+            switch (format)
+            {
+                case ExcelFormat.xls:
+                    workbook = new HSSFWorkbook();
+                    break;
+                case ExcelFormat.xlsx:
+                    workbook = new XSSFWorkbook();
+                    break;
+                default:
+                    workbook = new HSSFWorkbook();
+                    break;
+            }
+
+            ISheet sheet = workbook.CreateSheet("Sheet0");//创建一个名称为Sheet0的表  
+            int rowCount = source.Rows.Count;//行数  
+            int columnCount = source.Columns.Count;//列数  
+
+            //设置列头  
+            IRow row = sheet.CreateRow(0);//excel第一行设为列头  
+            for (int c = 0; c < columnCount; c++)
+            {
+                ICell cell = row.CreateCell(c);
+                cell.SetCellValue(source.Columns[c].ColumnName);
+            }
+
+            //设置每行每列的单元格,  
+            for (int i = 0; i < rowCount; i++)
+            {
+                row = sheet.CreateRow(i + 1);
+                for (int j = 0; j < columnCount; j++)
+                {
+                    ICell cell = row.CreateCell(j);//excel第二行开始写入数据
+                    string defaultValue = source.Rows[i][j].ToString();//默认为字符串
+                    switch (source.Columns[j].DataType.ToString())
+                    {
+                        case "System.String"://字符串类型
+                            cell.SetCellValue(defaultValue);
+                            break;
+                        case "System.DateTime"://日期类型
+                            //System.DateTime dateV;
+                            //System.DateTime.TryParse(defaultValue, out dateV);
+                            cell.SetCellValue(defaultValue);
+                            break;
+                        case "System.Boolean"://布尔型
+                            bool boolV = false;
+                            bool.TryParse(defaultValue, out boolV);
+                            cell.SetCellValue(boolV);
+                            break;
+                        case "System.Int16"://整型
+                        case "System.Int32":
+                        case "System.Int64":
+                        case "System.Byte":
+                            int intV = 0;
+                            int.TryParse(defaultValue, out intV);
+                            cell.SetCellValue(intV);
+                            break;
+                        case "System.Decimal"://浮点型
+                        case "System.Double":
+                            double doubV = 0;
+                            double.TryParse(defaultValue, out doubV);
+                            cell.SetCellValue(doubV);
+                            break;
+                        case "System.DBNull"://空值处理
+                            cell.SetCellValue(string.Empty);
+                            break;
+                        default:
+                            cell.SetCellValue(defaultValue);
+                            break;
+                    }
+                }
+            }
+
+            var ms = new NpoiMemoryStream();
+            ms.AllowClose = false;
+            workbook.Write(ms);
+            ms.Flush();
+            ms.Position = 0;
+            ms.AllowClose = false;
+            return ms;
+        }
+
+        /// <summary>
+        /// 读取Excel到List<T>(NPOI方式)
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="fullFilePath">文件的完整物理路径</param>
+        /// <returns>List<T></returns>
+        public static List<T> ReadExcelToList<T>(string fullFilePath) where T : class, new()
+        {
+            return ReadExcelToTable(fullFilePath).ToList<T>();
+        }
+
+        /// <summary>
+        /// 读取Excel到List<T>(NPOI方式)
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="file">含有Excel的文件流</param>
+        /// <param name="format">文件扩展名（.xls,.xlsx）</param>
+        /// <returns>List<T></returns>
+        public static List<T> ReadExcelToList<T>(Stream file, ExcelFormat format) where T : class, new()
+        {
+            return ReadExcelToTable(file, format).ToList<T>();
+        }
+
+        /// <summary>
+        /// 读取Excel到List(NPOI方式)
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="file">含有Excel的文件</param>
+        /// <returns>DataTable</returns>
+        public static List<T> ReadExcelToList<T>(IFormFile file) where T : class, new()
+        {
+            return ReadExcelToTable(file).ToList<T>();
+        }
+
+        /// <summary>
+        /// 读取List<T>数据源到Excel内存流(NPOI方式)
+        /// 1.写入到Excel文件，2.输出到响应
+        /// 3.用完之后记得释放（using{}）
+        /// </summary>
+        /// <param name="source">数据源</param>
+        /// <param name="format">文件扩展名（.xls,.xlsx）</param>
+        /// <returns>数据流</returns>
+        public static MemoryStream ReadListToExcel<T>(List<T> source, ExcelFormat format = ExcelFormat.xls) where T : class, new()
+        {
+            return ReadDataTableToExcel(source.ToDataTable(), format);
+        }
+
+        /// <summary>
+        /// 从Excel的一个工作簿获取DataTable(默认获取第一个Sheet)
+        /// </summary>
+        /// <param name="workbook">工作簿</param>
+        /// <param name="sheetIndex">Sheet索引</param>
+        /// <returns>DataTable</returns>
+        private static DataTable GetDataTableFromWorkbook(IWorkbook workbook, int sheetIndex = 0)
+        {
+            if (workbook == null)
+                throw new FileNotFoundException("The excel file do not exist!");
+
+            return GetDataTableFromSheet(workbook.GetSheetAt(sheetIndex));
+        }
+
+        /// <summary>
+        /// 从Excel的一个Sheet获取DataTable(第一行为表头,不记录数据)
+        /// </summary>
+        /// <param name="sheet">Excel的Sheet</param>
+        /// <returns>DataTable</returns>
+        private static DataTable GetDataTableFromSheet(ISheet sheet)
+        {
+            if (sheet == null)
+                throw new FileNotFoundException("The excel file do not exist!");
+
+            DataTable dt = new DataTable();
+
+            IRow headerRow = sheet.GetRow(0);
+            int cellCount = headerRow.LastCellNum;
+
+            for (int j = 0; j < cellCount; j++)
+            {
+                ICell cell = headerRow.GetCell(j);
+                dt.Columns.Add(cell?.ToString());
+            }
+
+            for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++)
+            {
+                IRow row = sheet.GetRow(i);
+                if (row == null || row.Cells.Count == 0)//跳过整行为空的情况
+                    continue;
+                DataRow dataRow = dt.NewRow();
+
+                for (int j = row.FirstCellNum; j < cellCount; j++)
+                {
+                    var cell = row.GetCell(j);
+                    if (cell == null)
+                        continue;
+
+                    #region 旧方法
+                    //CellType(Unknown = -1,Numeric = 0,String = 1,Formula = 2,Blank = 3,Boolean = 4,Error = 5,)  
+                    //switch (cell.CellType)
+                    //{
+                    //    case CellType.Blank:
+                    //        dataRow[j] = string.Empty;
+                    //        break;
+                    //    case CellType.Numeric:
+                    //        short cellFormat = cell.CellStyle.DataFormat;
+                    //        //对时间格式（2015.12.5、2015/12/5、2015-12-5等）的处理  
+                    //        if (cellFormat == 14 || cellFormat == 31 || cellFormat == 57 || cellFormat == 58)
+                    //            dataRow[j] = cell.DateCellValue;
+                    //        else
+                    //            dataRow[j] = cell.NumericCellValue;
+                    //        break;
+                    //    case CellType.String:
+                    //        dataRow[j] = cell.StringCellValue;
+                    //        break;
+                    //    default:
+                    //        dataRow[j] = cell.ToString();
+                    //        break;
+                    //} 
+                    #endregion
+
+                    switch (cell.CellType)
+                    {
+                        case CellType.Unknown:
+                            dataRow[j] = cell.ToString();
+                            break;
+                        case CellType.Numeric:
+                            dataRow[j] = DateUtil.IsCellDateFormatted(cell) ? cell.DateCellValue.ToString() : cell.NumericCellValue.ToString();
+                            break;
+                        case CellType.String:
+                            dataRow[j] = cell.StringCellValue;
+                            break;
+                        case CellType.Formula:
+                            try
+                            {
+                                HSSFFormulaEvaluator e = new HSSFFormulaEvaluator(cell.Sheet.Workbook);
+                                e.EvaluateInCell(cell);
+                                dataRow[j] = cell.ToString();
+                            }
+                            catch
+                            {
+                                dataRow[j] = cell.NumericCellValue.ToString();
+                            }
+                            break;
+                        case CellType.Blank:
+                            dataRow[j] = string.Empty;
+                            break;
+                        case CellType.Boolean:
+                            dataRow[j] = cell.BooleanCellValue.ToString();
+                            break;
+                        case CellType.Error:
+                            dataRow[j] = cell.ErrorCellValue.ToString();
+                            break;
+                        default:
+                            dataRow[j] = cell.ToString();
+                            break;
+                    }
+                }
+                dt.Rows.Add(dataRow);
+            }
+            return dt;
+        }
+    }    /// <summary>
+         /// Excel文件帮助类，通过NPOI进行操作
+         /// </summary>
     public static class ExcelHelper
     {
         /// <summary>
