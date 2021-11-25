69007d6ac997ce74b5ae65737f61c9a0b332afa3
newcats <newcats@live.com>
2021-11-26 00:07:28 +0800

    修改文件夹名称

--- src/Newcats.DataAccess.MySQL/IRepository.cs
+++ src/Newcats.DataAccess.MySQL/IRepository.cs
@@ -1,7 +1,7 @@
 ﻿namespace Newcats.DataAccess.MySql
 {
     /// <summary>
-    /// MySQL仓储接口,提供数据库访问能力,封装了基本的CRUD方法。
+    /// MySql仓储接口,提供数据库访问能力,封装了基本的CRUD方法。
     /// </summary>
     /// <typeparam name="TDbContext">数据库上下文，不同的数据库连接注册不同的DbContext</typeparam>
     public interface IRepository<TDbContext> : Core.IRepository<TDbContext> where TDbContext : Core.IDbContext
