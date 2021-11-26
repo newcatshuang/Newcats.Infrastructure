2f87a195773ee41784bdf59a16ecea978a54cc08
newcats <newcats@live.com>
2021-11-26 14:47:41 +0800

    修改mysql为小写

---
+++ src/Newcats.DataAccess.MySql/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Newcats.DataAccess.MySql
+{
+    /// <summary>
+    /// MySql仓储接口,提供数据库访问能力,封装了基本的CRUD方法。
+    /// </summary>
+    /// <typeparam name="TDbContext">数据库上下文，不同的数据库连接注册不同的DbContext</typeparam>
+    public interface IRepository<TDbContext> : Core.IRepository<TDbContext> where TDbContext : Core.IDbContext
+    {
+    }
+}
