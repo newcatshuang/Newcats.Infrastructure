0ebfc296175a746564f12212dde04ff7ab8dc1a3
newcats <newcats@live.com>
2021-12-02 10:33:05 +0800

    增加集合的扩展方法IsNullOrEmpty

--- src/Newcats.Utils/Extensions/CollectionExtensions.cs
+++ src/Newcats.Utils/Extensions/CollectionExtensions.cs
@@ -239,5 +239,16 @@ namespace Newcats.Utils.Extensions
             if (collection == null || !collection.Any())
                 throw new ArgumentNullException(nameof(collection), "The collection is null or empty.");
         }
+
+        /// <summary>
+        /// Indicates whether the specified collection is null or empty.
+        /// </summary>
+        /// <typeparam name="T">The type of objects to enumerate</typeparam>
+        /// <param name="collection">The collection</param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
+        {
+            return collection == null || !collection.Any();
+        }
     }
 }
