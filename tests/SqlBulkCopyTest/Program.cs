b6b5778689bc7ad41026bce1f1cb82b7fa579f39
newcats <newcats@live.com>
2021-12-23 21:28:00 +0800

    添加BenchmarkDotNet测试

--- tests/SqlBulkCopyTest/Program.cs
+++ tests/SqlBulkCopyTest/Program.cs
@@ -1,4 +1,7 @@
-﻿using Newcats.Utils.Helpers;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using Newcats.Utils.Helpers;
 
 namespace SqlBulkCopyTest;
 
@@ -6,29 +9,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        int TotalCount = 50;
-        Console.Write("输入需要测试的数据量：");
-        string countStr = Console.ReadLine();
-        Console.WriteLine("=========================================");
-        int outer = 0;
-        if (int.TryParse(countStr, out outer))
-            TotalCount = outer;
-
-        List<NewcatsUserInfoTest> list = new List<NewcatsUserInfoTest>();
-        for (int i = 0; i < TotalCount; i++)
+        Console.WriteLine("1.使用for循环测试SqlServer、MySql、ostgreSql");
+        Console.WriteLine("2.使用BenchmarkDotNet测试SqlServer的InsertForEach、SqlBulkCopy");
+        string select = Console.ReadLine();
+        if (select == "1")
         {
-            NewcatsUserInfoTest u = new NewcatsUserInfoTest()
-            {
-                Id = IdHelper.Create(),
-                Name = EncryptHelper.GetRandomString(Random.Shared.Next(20)),
-                CreateTime = DateTime.Now
-            };
-            list.Add(u);
-        }
+            int TotalCount = 50;
+            Console.Write("输入需要测试的数据量：");
+            string countStr = Console.ReadLine();
+            Console.WriteLine("=========================================");
+            int outer = 0;
+            if (int.TryParse(countStr, out outer))
+                TotalCount = outer;
 
-        RunSqlServerTest(list);
-        RunMySqlTest(list);
-        RunPostgreSqlTest(list);
+            List<NewcatsUserInfoTest> list = new List<NewcatsUserInfoTest>();
+            for (int i = 0; i < TotalCount; i++)
+            {
+                NewcatsUserInfoTest u = new NewcatsUserInfoTest()
+                {
+                    Id = IdHelper.Create(),
+                    Name = EncryptHelper.GetRandomString(Random.Shared.Next(20)),
+                    CreateTime = DateTime.Now
+                };
+                list.Add(u);
+            }
+
+            RunSqlServerTest(list);
+            RunMySqlTest(list);
+            RunPostgreSqlTest(list);
+        }
+        else if (select == "2")
+        {
+            Summary summary = BenchmarkRunner.Run<BulkCopyContext>();
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Hello World");
+        }
     }
 
     static void RunSqlServerTest(List<NewcatsUserInfoTest> list)
@@ -89,5 +107,57 @@ class Program
         Console.WriteLine("\r\nPostgreSql测试结果如下：");
         Console.WriteLine($"集合大小:{list.Count}\r\n\n1.InsertForEach方法耗时:{t1}ms\r\n\n2.InsertForEachNative方法耗时:{t2}ms\r\n\n3.InsertBulk方法耗时:{t3}ms\r\n\n4.InsertAppend方法耗时:{t4}ms\r\n\n5.SqlBulkCopy方法耗时:{t5}ms");
         Console.WriteLine("=========================================");
+    }
+
+
+}
+
+/// <summary>
+/// SqlBulkCopy测试
+/// </summary>
+public class BulkCopyContext
+{
+    /// <summary>
+    /// ForEach
+    /// </summary>
+    [Benchmark]
+    public void InsertForEach()
+    {
+        List<NewcatsUserInfoTest> list = new List<NewcatsUserInfoTest>();
+        for (int i = 0; i < 10; i++)
+        {
+            NewcatsUserInfoTest u = new NewcatsUserInfoTest()
+            {
+                Id = IdHelper.Create(),
+                Name = EncryptHelper.GetRandomString(Random.Shared.Next(20)),
+                CreateTime = DateTime.Now
+            };
+            list.Add(u);
+        }
+        SqlServerTest sqlServer = new SqlServerTest();
+        sqlServer.Init();
+        sqlServer.InsertForEach(list);
+    }
+
+    /// <summary>
+    /// SqlBulkCopy
+    /// </summary>
+    [Benchmark]
+    public void SqlBulkCopy()
+    {
+        List<NewcatsUserInfoTest> list = new List<NewcatsUserInfoTest>();
+        for (int i = 0; i < 10; i++)
+        {
+            NewcatsUserInfoTest u = new NewcatsUserInfoTest()
+            {
+                Id = IdHelper.Create(),
+                Name = EncryptHelper.GetRandomString(Random.Shared.Next(20)),
+                CreateTime = DateTime.Now
+            };
+            list.Add(u);
+        }
+        SqlServerTest sqlServer = new SqlServerTest();
+        sqlServer.Init();
+        sqlServer.SqlBulkCopy(list);
     }
 }
