ac25ec1b2cf9b5f277f8e7ad63bbd63baa3d974f
newcats <newcats@live.com>
2024-05-19 21:57:17 +0800

    WIP：暂存

---
+++ tests/HostApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using GeneratorApp;
+
+namespace HostApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class SendMessage : ISendMessage
+    {
+        public string Message { get; set; }
+    }
+
+    [DIAttribute]
+    public interface ISendMessage
+    {
+
+    }
+}
